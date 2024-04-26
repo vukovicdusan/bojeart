5859e4aea62662973d773a9c8c0ca4cd36713f44
Picvajzmen <picvajzmen@gmail.com>
2024-04-26 11:34:23 +0200

    added google site verification

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,45 @@
-import "../styles/globals.css"
-import Layout from "../components/layout/Layout"
-import { LoginContextProvider } from "../store/LoginCtx"
-import Head from "next/head"
+import "../styles/globals.css";
+import Layout from "../components/layout/Layout";
+import { LoginContextProvider } from "../store/LoginCtx";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<>
-			<Head>
-				<link
-					rel="preload"
-					href="/fonts/amaticsc-bold-webfont.woff2"
-					as="font"
-					type="font/woff2"
-					crossOrigin="anonymous"
-				/>
-				<link
-					rel="preload"
-					href="/fonts/amaticsc-regular-webfont.woff2"
-					as="font"
-					type="font/woff2"
-					crossOrigin="anonymous"
-				/>
-				<link
-					rel="preload"
-					href="/fonts/comforter-regular-webfont.woff2"
-					as="font"
-					type="font/woff2"
-					crossOrigin="anonymous"
-				/>
-			</Head>
-			<LoginContextProvider>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			</LoginContextProvider>
-		</>
-	)
+  return (
+    <>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/amaticsc-bold-webfont.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/amaticsc-regular-webfont.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/comforter-regular-webfont.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <meta
+          name="google-site-verification"
+          content="EjsFsgqPf65CBx--tDgvLgIGhKumHZZ50FBePUZFt-4"
+        />
+      </Head>
+      <LoginContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </LoginContextProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
